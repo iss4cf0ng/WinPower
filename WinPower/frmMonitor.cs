7eda519d22896f8d3751742663835cee938be7a3
malbuffer4pt <icbluewhale@gmail.com>
2026-06-08 15:50:55 +0800

    update

---
+++ WinPower/frmMonitor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinPower
+{
+    public partial class frmMonitor : Form
+    {
+        public frmMonitor()
+        {
+            InitializeComponent();
+        }
+
+        void fnSetup()
+        {
+
+        }
+
+        private void frmMonitor_Load(object sender, EventArgs e)
+        {
+            fnSetup();
+        }
+    }
+}
